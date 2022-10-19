f455ddbfa335af377bd217e49170da7646d49675
EvilR2D2 <diegoalaniz7575@gmail.com>
2022-10-19 09:53:59 -0500

    add  an array that allows me to randomly pick one of three options

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let value1 = "one";
+// let value1 = "one";
 
-let value2 = "two";
+// let value2 = "two";
 
-var chosenValue = Math.random() < 0.5 ? value1 : value2;
+// var chosenValue = Math.random() < 0.5 ? value1 : value2;
 
-console.log(chosenValue)
+//////////////////
+
+let result = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+
+console.log(result)
