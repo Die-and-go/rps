4146075976a3281ffa79fd548f7224f718f0e4b8
EvilR2D2 <diegoalaniz7575@gmail.com>
2022-12-31 11:43:25 -0600

    final touches

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-
-
-
-//Function that gets a random variable (COMPLETE)
-function getComputerChoice() {
-
-let computerSelection = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
-
-return computerSelection;
-}
-
-console.log(getComputerChoice())
-
-
-//Function that allows a player to type and enter a variable
-function getPlayerChoice() {
-
-    let playerSelection = prompt("Select rock, paper, or scissors");
-
-    if(playerSelection == "rock") {
-        console.log("you chose rock")
-    } else if(playerSelection == "paper") {
-        console.log ("you chose paper")
-    } else if(playerSelection == "scissors") {
-        console.log("you chose scissors")
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]')
+const SELECTIONS = [
+    {
+    name: 'rock',
+    emoji: '✊',
+    beats: 'scissors'
+    },
+    {
+    name: 'paper',
+    emoji: '✋',
+    beats: 'rock'
+    },
+    {
+    name: 'scissors',
+    emoji: '🤘',
+    beats: 'paper'
     }
+] 
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
 }
 
-console.log(getPlayerChoice())
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div');
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
